16fe40dfe38921c75c41ea4714022dad7a1c3667
bytebutcher <thomas.engel.web@gmail.com>
2020-03-28 02:20:06 +0100

    Fixed syntax error due to incomplete merge

--- bdb.sh
+++ bdb.sh
@@ -39,7 +39,7 @@ function add_bookmark() {
 		echo "ERROR: Invalid bookmark location!" >&2
 		exit 1
 	fi
-	if [ grep "^${1}\$" "${BOOKMARK_FILE}" ] ; then
+	if grep "^${1}\$" "${BOOKMARK_FILE}" &> /dev/null ; then
 		echo "Location '${1}' is already bookmarked." >&2
 		exit 0
 	fi
@@ -59,7 +59,7 @@ function get_bookmark_by_index() {
 
 function remove_bookmark() {
 	if [ -f "${BOOKMARK_FILE}" ] ; then
-		if ! [ grep "^${1}\$" "${BOOKMARK_FILE}" ] ; then
+		if ! grep "^${1}\$" "${BOOKMARK_FILE}" &> /dev/null ; then
 			echo "No matching boomark found." >&2
 			exit 0
 		fi
@@ -93,7 +93,7 @@ fi
 
 case ${1} in
 	-a | --add)
-		if [ -z "${path}" ] ; then
+		if [ -z "${2}" ] ; then
 			path="${CURRENT_PATH}"
 		else
 			path="${2}"
@@ -102,8 +102,8 @@ case ${1} in
 		exit 0
 		;;
 	-r | --remove)
-		index="${2}"
-		if [ -n "${index}" ] ; then
+		if [ -n "${2}" ] ; then
+			index="${2}"
 			path=$(get_bookmark_by_index "${index}")
 		else
 			path="${CURRENT_PATH}"
